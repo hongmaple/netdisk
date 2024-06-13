d60722f74468d4c78fda6bed7aac03c279433ab0
maple <zhuzhihong@anjiu-tech.com>
2024-06-13 12:43:18 +0800

    修改前端访问根目录

--- web-ui/src/router/index.js
+++ web-ui/src/router/index.js
@@ -187,5 +187,6 @@ Router.prototype.push = function push(location) {
 export default new Router({
   mode: 'history', // 去掉url中的#
   scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
+  routes: constantRoutes,
+  base: "/netdisk"
 })
