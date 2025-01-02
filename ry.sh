e4d794b7118ab5a07359b6add618cdd9a23bced9
maple <zhuzhihong@anjiu-tech.com>
2025-01-02 12:05:08 +0800

    网盘服务监控，自动启动，与钉钉通知，和邮件通知
    (cherry picked from commit 1d3c889f431fda758e8269881e7e7bd04e4b604d)

--- ry.sh
+++ ry.sh
@@ -1,15 +1,14 @@
-#!/bin/sh
-# ./ry.sh start 启动 stop 停止 restart 重启 status 状态
+
 AppName=ruoyi-admin.jar
 
-# JVM参数
+
 JVM_OPTS="-Dname=$AppName  -Duser.timezone=Asia/Shanghai -Xms512m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
 APP_HOME=`pwd`
 LOG_PATH=$APP_HOME/logs/$AppName.log
 
 if [ "$1" = "" ];
 then
-    echo -e "\033[0;31m 未输入操作名 \033[0m  \033[0;34m {start|stop|restart|status} \033[0m"
+    echo -e "\033[0;31m 未输入操作名 \033[0m  \033[0;34m {start|stop|restart|status|monitor_service} \033[0m"
     exit 1
 fi
 
@@ -72,6 +71,24 @@ function status()
     fi
 }
 
+function monitor_service() {
+    echo "monitor_service start"
+    while true; do
+        PID=`ps -ef |grep java|grep $AppName|grep -v grep|wc -l`
+        if [ $PID != 0 ];then
+          echo "monitor 正常"
+        else
+            start
+            # /opt/notify.sh "服务状态通知" "服务已挂掉并自动重启。" email
+            /opt/notify.sh "网盘服务状态通知" "服务已挂掉并自动重启。" dingtalk
+            echo "$(date) - Service $AppName is not running. Restarting..." >> $LOG_PATH
+        fi
+
+        sleep 60
+    done
+}
+
+
 case $1 in
     start)
     start;;
@@ -81,6 +98,8 @@ case $1 in
     restart;;
     status)
     status;;
+    monitor_service)
+    monitor_service;;
     *)
 
 esac
